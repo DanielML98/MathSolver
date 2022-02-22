14096006b1460ab78b6d4a0b1860664dbcc2cdb5
Daniel Martínez <danielml@MacBook-Air-de-Daniel.local>
2022-02-22 00:17:40 -0600

    Added spinner functionality, view binding and splash screen

--- app/src/main/java/com/danielml/mathsolver/MainActivity.kt
+++ app/src/main/java/com/danielml/mathsolver/MainActivity.kt
@@ -2,10 +2,45 @@ package com.danielml.mathsolver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
+import com.danielml.mathsolver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    Thread.sleep(3000)
+    setTheme(R.style.Theme_MathSolver)
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    setUpSpinner()
+  }
+
+  fun setUpSpinner() {
+    ArrayAdapter.createFromResource(
+      this,
+      R.array.equations,
+      android.R.layout.simple_spinner_dropdown_item
+    ).also {
+      it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+      binding.spinner.adapter = it
     }
+    binding.spinner.onItemSelectedListener = this
+  }
+
+  override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    when(parent?.selectedItemPosition) {
+      0 -> binding.equationImageView.setImageResource(R.drawable.quadratic)
+      1 -> binding.equationImageView.setImageResource(R.drawable.triangle_area)
+      2 -> binding.equationImageView.setImageResource(R.drawable.area_rectangle)
+    }
+  }
+
+  override fun onNothingSelected(parent: AdapterView<*>?) {}
+
 }
